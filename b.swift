58fbe1bff29af1dc8fb7a9f13c495fb45502c7a7
Michal Karwanski <karwer@codequest.com>
2017-09-17 12:17:02 +0200

    Larger function

---
+++ b.swift
@@ -0,0 +1,30 @@
+class Test {
+
+func testWhile() {
+
+print("a")
+print("b")
+print("c")
+print("d")
+print("e")
+print("f")
+print("g")
+print("h")
+
+while 1 == 1 {
+  while 1 == 1 {
+    while 1 == 1 {
+      while 1 == 1 {
+        while 1 == 1 {
+          while 1 == 1 {
+          }
+        }
+      }
+    }
+  }
+} 
+
+}
+
+}
+
