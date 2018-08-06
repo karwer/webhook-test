67d5e71046b2bea44ea63c4e6f682be2a1a3f1b6
Michal Karwanski <karwer@gmail.com>
2018-08-06 14:42:16 +0200

    Add G file

---
+++ g.swift
@@ -0,0 +1,43 @@
+class TestG {
+
+func testWhile() {
+
+print("a")
+print("b")
+print("c")
+print("d")
+print("e")
+print("f")
+print("g")
+print("h")
+
+while 1 == 1 {
+  while 1 == 1 {
+    while 1 == 1 {
+      while 1 == 1 {
+        while 1 == 1 {
+          while 1 == 1 {
+          }
+        }
+      }
+    }
+  }
+} 
+
+if true {
+  if true {
+    if true {
+      while 1 == 1 {
+        if true {
+          while 1 == 1 {
+          }
+        }
+      }
+    }
+  }
+}
+
+}
+
+}
+
